21248b5a1f97196de7287ef36a7c4819360ba75a
Anna Przybycien <ania.przybycien@gmail.com>
2020-11-22 23:10:39 +0100

    Move code around and clean up in general

---
+++ doggos-training.py
@@ -0,0 +1,30 @@
+from xml.etree import ElementTree
+from os import listdir
+from xml.etree import ElementTree
+from numpy import zeros
+from numpy import asarray
+from mrcnn.utils import Dataset
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from doggosutils import DoggosDataset, DoggosConfig
+import re
+
+# prepare train set
+train_set = DoggosDataset()
+train_set.load_dataset('dogs', is_train=True)
+train_set.prepare()
+print('Train: %d' % len(train_set.image_ids))
+# prepare test/val set
+test_set = DoggosDataset()
+test_set.load_dataset('dogs', is_train=False)
+test_set.prepare()
+print('Test: %d' % len(test_set.image_ids))
+# prepare config
+config = DoggosConfig()
+config.display()
+# define the model
+model = MaskRCNN(mode='training', model_dir='./', config=config)
+# load weights (mscoco) and exclude the output layers
+model.load_weights('mask_rcnn_coco.h5', by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
+# train weights (output layers or 'heads')
+model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=5, layers='heads')
