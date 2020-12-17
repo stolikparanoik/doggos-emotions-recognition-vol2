3d7ca2e0b9cfb76d6d80c17561252b3506216bc1
Anna Przybycień <aprzybycien@atlassian.com>
2020-12-17 10:32:57 +0100

    add doggos emotion classification training

---
+++ emotions-training.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras import layers
+from keras import models
+from keras.callbacks import EarlyStopping
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3),
+                        activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(4, activation='softmax'))
+model.summary()
+
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2, zoom_range=0.2,
+                                   horizontal_flip=True,)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+original_dataset_dir = './data/train'
+base_dir = './split_data'
+# os.mkdir(base_dir)
+
+train_dir = os.path.join(base_dir, 'train')
+# os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+# os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical'
+)
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+for data_batch, labels_batch in train_generator:
+    print('data batch shape: ', data_batch.shape)
+    print('labels batch shape: ', labels_batch.shape)
+    break
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=60,
+    validation_data=validation_generator,
+    validation_steps=50,
+    callbacks=[es]
+)
+model.save('modelcool60.h5')
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validating loss')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.legend()
+
+plt.show()
